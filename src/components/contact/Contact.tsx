b3b50ccb4d581e44f1e3cb89238276ec7080c843
arapbaeva <aiymvector@gmail.com>
2023-03-03 03:54:45 +0600

    added emailjs for contact

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -3,42 +3,60 @@ import s from '../../App.module.css'
 import style from './Contact.module.css'
 import {MdOutlineEmail} from "react-icons/md";
 import {BsTelegram, BsWhatsapp} from "react-icons/bs";
+import emailjs from 'emailjs-com'
 
 export const Contact = () => {
-    return (
-<section id={'contact'}>
-    <h5>Get In Touch</h5>
-    <h2>Contact Me</h2>
 
-    <div className={`${s.container} ${style.contactContainer}`}>
-        <div className={style.contactOptions}>
-            <article className={style.contactOption}>
-                <MdOutlineEmail className={style.contactOptionIcon}/>
-                <h4>Email</h4>
-                <h5>aiymvector@gmail.com</h5>
-                <a href="mailto:aiymvector@gmail.com" target='_blank'>Send a message</a>
-            </article>
-            <article className={style.contactOption}>
-                <BsTelegram className={style.contactOptionIcon}/>
-                <h4>Telegram</h4>
-                <h5>aiymvector</h5>
-                <a href="https://t.me/vectoromela" target='_blank'>Send a message</a>
-            </article>
-            <article className={style.contactOption}>
-                <BsWhatsapp className={style.contactOptionIcon}/>
-                <h4>WhatsApp</h4>
-                <h5>+996704777050</h5>
-                <a href="https://api.whatsapp.com/send?phone+996704777050" target='_blank'>Send a message</a>
-            </article>
-        </div>
-        <form action="">
-            <input type="text" name={'name'} placeholder={'Your full Name'} required/>
-            <input type="email" name={'email'} placeholder={'Your Email'} required/>
-            <textarea name="message"  rows={7} placeholder={'your Message'} required></textarea>
-            <button type={"submit"} className={`${s.btn} ${s.btnPrimary}`}>Send Message</button>
-        </form>
-    </div>
-</section>
+    const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm("service_2g0nehr", "template_43vnr3n", e.currentTarget, "D4qrSLNPKSnIAKMta")
+            .then(
+                (result) => {
+                    console.log(result.text);
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
+        e.currentTarget.reset()
+    }
+
+    return (
+        <section id={'contact'}>
+            <h5>Get In Touch</h5>
+            <h2>Contact Me</h2>
+
+            <div className={`${s.container} ${style.contactContainer}`}>
+                <div className={style.contactOptions}>
+                    <article className={style.contactOption}>
+                        <MdOutlineEmail className={style.contactOptionIcon}/>
+                        <h4>Email</h4>
+                        <h5>aiymvector@gmail.com</h5>
+                        <a href="mailto:aiymvector@gmail.com" target='_blank'>Send a message</a>
+                    </article>
+                    <article className={style.contactOption}>
+                        <BsTelegram className={style.contactOptionIcon}/>
+                        <h4>Telegram</h4>
+                        <h5>aiymvector</h5>
+                        <a href="https://t.me/vectoromela" target='_blank'>Send a message</a>
+                    </article>
+                    <article className={style.contactOption}>
+                        <BsWhatsapp className={style.contactOptionIcon}/>
+                        <h4>WhatsApp</h4>
+                        <h5>+996704777050</h5>
+                        <a href="https://api.whatsapp.com/send?phone+996704777050" target='_blank'>Send a message</a>
+                    </article>
+                </div>
+                <form onSubmit={sendEmail}>
+                    <input type="text" name={'name'} placeholder={'Your full Name'} required/>
+                    <input type="email" name={'email'} placeholder={'Your Email'} required/>
+                    <textarea name="message" rows={7} placeholder={'your Message'} required></textarea>
+                    <button type={"submit"} className={`${s.btn} ${s.btnPrimary}`}>Send Message</button>
+                </form>
+            </div>
+        </section>
     );
 };
 

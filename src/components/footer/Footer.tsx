d65ff05222c6432e39a58df11640ecdc26e65ae5
arapbaeva <aiymvector@gmail.com>
2023-03-03 08:05:01 +0600

    added footer component and styles for it

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import style from './Footer.module.css'
+import s from '../../App.module.css'
+
 
 export const Footer = () => {
     return (
-        <div>
-footer
-        </div>
+        <footer>
+            <ul className={style.permalinks}>
+                <li><a href={"#"}>Home</a></li>
+                <li><a href={"#experience"}>Experience</a></li>
+                <li><a href={"#portfolio"}>Portfolio</a></li>
+                <li><a href={"#contact"}>Contact</a></li>
+            </ul>
+
+            <div className={style.footerCopyRight}>
+                <small>Copyright &copy; 2023. All Rights Reserved.</small>
+            </div>
+        </footer>
     );
 };
 
--- src/components/header/HeaderSocials.tsx
+++ src/components/header/HeaderSocials.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {FaGithub} from "react-icons/fa";
 import style from './Header.module.css'
+import {SiCodewars} from "react-icons/si";
 
 
 export const HeaderSocials = () => {
     return (
         <div className={style.headerSocials}>
-            <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-            <a href="https://github.com" target="_blank"><FaGithub/></a>
+            <a href="https://www.linkedin.com/in/aiym-ar-122a92238/" target="_blank"><BsLinkedin/></a>
+            <a href="https://github.com/arapbaeva" target="_blank"><FaGithub/></a>
+            <a href="https://www.codewars.com/dashboard" target="_blank"><SiCodewars/></a>
         </div>
     )
 
